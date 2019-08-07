e30f1d8660e6e324796a8449bee725145664b71f
neuroelf <cj.hammond.gm@gmail.com>
2019-08-07 20:10:43 +0000

    Made the mongod server start with nohup.

--- mern-backend/dm_mongo.sh
+++ mern-backend/dm_mongo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 DBPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-mongod --dbpath ${DBPATH}/mongodb
+nohup mongod --dbpath ${DBPATH}/mongodb > ${DBPATH}/mongodb/dm_mongo.log 2>&1 &
+
