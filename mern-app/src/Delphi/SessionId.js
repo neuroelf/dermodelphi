2b275ac4784ac213b825a1ff320219088fe35364
neuroelf <cj.hammond.gm@gmail.com>
2019-08-09 12:32:38 -0400

    Now detects <ENTER> to check session ID.

--- mern-app/src/Delphi/SessionId.js
+++ mern-app/src/Delphi/SessionId.js
@@ -8,6 +8,7 @@ export default class DelphiSessionId extends Component {
         this.state = { };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleKeyUp = this.handleKeyUp.bind(this);
     }
     
     handleSubmit(event) {
@@ -16,12 +17,18 @@ export default class DelphiSessionId extends Component {
     handleChange(event) {
         this.props.AppObj.setState({ sessionId: event.target.value });
     }
+    handleKeyUp(event) {
+        if (event.keyCode === 13) {
+            this.props.AppObj.checkSession(event);
+        }
+    }
     
     render() {
         const AppObj = this.props.AppObj;
         return (
             <input type="text" placeholder={SESS_PROMPT_ID}
-                value={AppObj.state.sessionId} onChange={this.handleChange} />
+                value={AppObj.state.sessionId}
+                onChange={this.handleChange} onKeyUp={this.handleKeyUp} />
         );
     }
 }
